463e5b671893fbb9707a00d026b52da53dbce51e
Navaneethkkkk <navaneethkk03@gmail.com>
2026-05-27 21:59:45 +0530

    tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Main from './pages/main'
+import LayoutRoutes from './Routes/LayoutRoutes'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+    <LayoutRoutes/>
     </>
   )
 }
---
+++ frontend/src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <>
+      <h1 className='bg-amber-300'>hekllo</h1>
+    </>
+  )
+}
+
+export default Main
+
